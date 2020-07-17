dc1d9d66d4feb6a1bae971f7a7494f1fa665753c
liamb <liamb@kobelt.com>
2020-07-16 17:03:48 -0700

    Added Percolation and PercolationStats classes.

--- Percolation.java
+++ Percolation.java
@@ -1,29 +1,43 @@
 /* *****************************************************************************
  *  Name:              Alan Turing
- *  Coursera User ID:  123456
+ *  Coursera User index:  123456
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
 public class Percolation {
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
+    private static final int CLOSED = 1;
+    private static final int OPEN = 0;
 
+    private int[] index;
+    private int[] size;
+    private int[] state;
+
+    // creates n-by-n index, with all sites initially blocked
+    public Percolation(int n) {
+        index = new int[n];
+
+        for (int i = 1; i < n+1; i++) {
+            index[i] = CLOSED;
+            size[i] = 0;
+            state[i] = CLOSED;
+        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-
+        if (index[row*col] == CLOSED)
+            index[row*col] = OPEN;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-
+        return index[row*col] == OPEN;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-
+        return index[row*col] == FULL;
     }
 
     // returns the number of open sites
